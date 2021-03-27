f89b2788c33985ebd0ecd0e774408a2644eeb7f9
Ivy Stephenson <istephensondev@gmail.com>
2021-03-27 15:49:11 -0500

    database

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS  musical_mecca;
+
+CREATE DATABASE  musical_mecca;
--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 var Sequelize = require('sequelize');
 var bcrypt = require('bcrypt');
 
-const sequelize = new Sequelize('ourDatabase', 'root', 'password', {
+const sequelize = new Sequelize('musical_mecca', 'root', 'password', {
     host: 'localhost',
     port: 3306,
     dialect: 'mysql',
